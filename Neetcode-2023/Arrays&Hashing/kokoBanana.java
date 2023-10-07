fd6e46cd8d52f75c09027b7da083593b844c2646
Rach1507 <rachitha715@gmail.com>
2023-10-07 16:22:21 +0530

    oct 7th

--- Neetcode-2023/Arrays&Hashing/kokoBanana.java
+++ Neetcode-2023/Arrays&Hashing/kokoBanana.java
@@ -1,55 +1,114 @@
 public class kokoBanana {
-    
-
-    //brute force
 
     public int minEatingSpeed(int[] piles, int h) {
 
-        
+        int low = 1;
 
-        int sum =0;
-        int max = 0 ;
+        int noOfHoursPassed = 0;
+        int max = 0,sum = 0;
         for(int i=0;i<piles.length;i++)
         {
-            if(piles[i] > max)max = piles[i];
+            if(piles[i] > max) max = piles[i];
             sum += piles[i];
 
         }
+        int currSolution  = max;
+        int high = max;
+        if (piles.length == h) return max;
 
-        int noOfHoursPassed = 0;
-        // int k = 1 ;
-        int k = sum/h; 
+        int i =0;
+        
+        while(low<=high){
 
-        int i = 0 ;
+            int mid = (low+high)/2;
 
-        int[] remainingPiles = piles.clone();
-
-        if(piles.length == h)return max;
+             while(noOfHoursPassed < h + 1 ){
 
 
-        while(1 <= k < max ){
+                if(i >= piles.length && noOfHoursPassed <= h){
 
-            while(noOfHoursPassed <= h ){
-
-                if(i >= piles.length){
-                    return k;
+                    currSolution = mid;
+                    high = mid-1;
+                    noOfHoursPassed=0;
+                    
+                    break;
                 }
 
-                remainingPiles[i] = remainingPiles[i] - k ;
-                if(remainingPiles[i] <= 0){
-                    i++;
+                if(piles[i]< mid)noOfHoursPassed++;
+                else{
+                noOfHoursPassed += ( piles[i] / mid );
+                if(piles[i] % mid > 0) noOfHoursPassed++;
                 }
-                
-                noOfHoursPassed++;
+                i++;
             }
-            noOfHoursPassed=0;
-            k++;
-            i=0;
-            remainingPiles = piles.clone();
 
+            if(noOfHoursPassed >= h && i <= piles.length){
+                low = mid+1;
+                high = currSolution;
+                noOfHoursPassed=0;
+            }
+
+            i=0;
         }
 
-    return max;
-       
+        return currSolution;
+
     }
 }
+
+
+    //brute force
+
+    // public int minEatingSpeed(int[] piles, int h) {
+
+        
+
+    //     int sum =0;
+    //     int max = 0 ;
+    //     for(int i=0;i<piles.length;i++)
+    //     {
+    //         if(piles[i] > max)max = piles[i];
+    //         sum += piles[i];
+
+    //     }
+
+    //     int noOfHoursPassed = 0;
+    //     // int k = 1 ;
+    //     int k = sum/h; 
+
+    //     int i = 0 ;
+
+    //     int[] remainingPiles = piles.clone();
+
+    //     if(piles.length == h)return max;
+
+
+    //     while(1 <= k < max ){
+
+    //         while(noOfHoursPassed <= h ){
+
+    //             if(i >= piles.length){
+    //                 return k;
+    //             }
+
+    //             remainingPiles[i] = remainingPiles[i] - k ;
+    //             if(remainingPiles[i] <= 0){
+    //                 i++;
+    //             }
+                
+    //             noOfHoursPassed++;
+    //         }
+    //         noOfHoursPassed=0;
+    //         k++;
+    //         i=0;
+    //         remainingPiles = piles.clone();
+
+    //     }
+
+    // return max;
+       
+    // }
+
+
+
+
---
+++ Neetcode-2023/Recursion/towerOfHanoi.java
@@ -0,0 +1,16 @@
+package Neetcode-2023.Recursion;
+
+public class towerOfHanoi {
+    
+    // 1 from a to c
+    // 2 from A TO  b
+    // 1 from c to b
+
+    // 1 a to b
+    // 2 from a to c
+    // 1 from b to c 
+    //3 from a to b 
+    // 1 from c to a 
+    // 2 from c to b 
+    // 1 from a to b 
+}
