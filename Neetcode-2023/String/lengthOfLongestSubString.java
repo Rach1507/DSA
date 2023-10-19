e7ffd97cac4aea88faa66065e499ee74fcbfbcac
Rach1507 <rachitha715@gmail.com>
2023-10-19 07:36:40 +0530

    oct 18th

---
+++ Neetcode-2023/String/lengthOfLongestSubString.java
@@ -0,0 +1,23 @@
+public class lengthOfLongestSubString {
+
+    public int lengthOfLongestSubstring(String s) {
+
+        HashMap<Character, Integer> charMap = new HashMap<>();
+        int maxwindowSize = 0, start = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char currChar = s.charAt(i);
+            if (!charMap.containsKey(currChar) || charMap.get(currChar) < start) {
+                charMap.put(currChar, i);
+                maxwindowSize = Math.max((i - start) + 1, maxwindowSize);
+            } else {
+                start = charMap.get(currChar) + 1;
+                maxwindowSize = Math.max((i - start) + 1, maxwindowSize);
+
+                charMap.put(currChar, i);
+
+            }
+        }
+        return maxwindowSize;
+    }
+}
