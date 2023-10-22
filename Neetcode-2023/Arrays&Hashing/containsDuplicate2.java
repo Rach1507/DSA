fe1a77400080c20abfea6e7126d7f87af2f3242a
Rach1507 <rachitha715@gmail.com>
2023-10-22 22:45:27 +0530

    22 oct

--- Neetcode-2023/Arrays&Hashing/containsDuplicate2.java
+++ Neetcode-2023/Arrays&Hashing/containsDuplicate2.java
@@ -1,23 +1,66 @@
 class Solution {
+
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
-        ArrayList<Integer> value;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                value = map.get(nums[i]);
-                for (int index : value) {
-                    if (Math.abs(i - index) <= k)
-                        return true;
-                }
-                value.add(i);
-            } else {
-                value = new ArrayList<>();
-                value.add(i);
+         HashMap<Integer,Integer> map = new HashMap<>();
+         int start = 0 , end = -1;
 
-                map.put(nums[i], value);
-            }
+         for(int z =0;z<=k;z++){
+            if(z >= nums.length){return false;}
+            end++;
+            if(map.containsKey(nums[z])) return true;
+            else map.put(nums[z], z);
+         }
+
+        while(end < nums.length-1){
+            end++;
+            map.remove(nums[start]);
+            start++;
+            if(map.containsKey(nums[end])) return true;
+            else map.put(nums[end], end);
         }
-        return false;
+
+    return false;
     }
+
+    //  public boolean containsNearbyDuplicate(int[] nums, int k) {
+        
+    //     int i ; 
+    //     int j ;
+        
+    //     for(int z =1;z<=k;z++){
+    //          i = 0 ;
+    //          j = i + z ;
+
+    //         while(j < nums.length){
+    //             if(nums[i] == nums[j]) return true;
+    //             i++;j++;
+    //         }
+    //     }
+
+    // return false;
+    // }
+
+
+    // public boolean containsNearbyDuplicate(int[] nums, int k) {
+    //     HashMap<Integer , ArrayList<Integer >> map = new HashMap<>();
+    //     ArrayList<Integer> value ; 
+
+    //     for(int i =0;i<nums.length;i++){
+    //         if(map.containsKey(nums[i])){
+    //             value = map.get(nums[i]);
+    //             for(int index: value){
+    //                 if(Math.abs(i - index) <= k)return true;
+    //             }
+    //             value.add(i);
+    //         }
+    //         else {
+    //             value = new ArrayList<>();
+    //             value.add(i);
+
+    //             map.put(nums[i],value);
+    //         }
+    //     }
+    // return false;
+    // }
 }
---
+++ Neetcode-2023/Arrays&Hashing/productOfArr.java
@@ -0,0 +1,69 @@
+import java.util.Arrays;
+
+public class productOfArr {
+      public int[] productExceptSelf(int[] nums) {
+        
+        // brute force
+        int ans[] = new int[nums.length];
+        int productOfAlltoLeft[] = new int[nums.length];
+        int productOfAlltoRight[] = new int[nums.length];
+        int nonZeroProductToLeft[] = new int[nums.length];
+
+        int productOfall = 1;
+
+        Arrays.fill(productOfAlltoLeft , 1);
+        Arrays.fill(productOfAlltoRight , 1);
+        Arrays.fill(nonZeroProductToLeft , 1);
+
+
+        // i can cache the product of all to right 
+
+        for(int i =0 ;i < nums.length; i++){
+            productOfall *= nums[i]; 
+        }
+
+        for(int i =0;i < nums.length; i++){
+            if(i > 0) 
+            productOfAlltoLeft[i] = productOfAlltoLeft[i-1] * nums[i-1];
+
+            // if(i > 0 && nums[i-1] != 0 && nums[i] != 0)
+            // nonZeroProductToLeft[i] = nonZeroProductToLeft[i-1] * nums[i-1];
+
+            // if(nums[i] == 0)
+            //      productOfAlltoRight[i] = productOfall / nonZeroProductToLeft[i];
+
+            //  else if (productOfAlltoLeft[i] == 0){
+            //     productOfAlltoRight[i] = productOfall / nonZeroProductToLeft[i];
+            //     productOfAlltoRight[i] /= nums[i];
+            // }
+            // else{
+                productOfAlltoRight[i] = productOfall / productOfAlltoLeft[i];
+                productOfAlltoRight[i] /= nums[i];
+            // }
+          
+        }
+
+        
+
+        for(int i =0 ;i < productOfAlltoLeft.length; i++){
+          System.out.print(productOfAlltoLeft[i]+ " "); 
+        }
+
+System.out.println();
+        for(int i =0 ;i < productOfAlltoRight.length; i++){
+          System.out.print(productOfAlltoRight[i]+ " "); 
+        }
+
+        ans[0] = productOfAlltoRight[0];
+        ans[nums.length- 1] = productOfAlltoLeft[nums.length- 1];
+
+        for(int i =1 ;i < nums.length- 1; i++){
+           ans[i] = productOfAlltoLeft[i] * productOfAlltoRight[i];
+        }
+
+        return ans;
+
+    }
+}
+
+}
