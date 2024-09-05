40d08b62bb78633acf158e7e862b268bdef604ac
Rach1507 <rachitha715@gmail.com>
2024-09-05 22:54:08 +0530

    5th Sept

--- Neetcode-2023/Arrays&Hashing/subArrSum.java
+++ Neetcode-2023/Arrays&Hashing/subArrSum.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class subArrSum {
     public int subarraySum(int[] nums, int k) {
         int totalNoOfSubarr = 0;
@@ -17,3 +19,75 @@ public class subArrSum {
         return totalNoOfSubarr;
     }
 }
+
+class SolutionFinalOOFN {
+    HashMap<Integer,Integer> map;
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, sumUntilNow = 0 ;
+        map = new HashMap<>();
+        map.put(0, 1); 
+
+        for(int i = 0;i<nums.length;i++){
+            sumUntilNow += nums[i];
+            if(map.containsKey(sumUntilNow - k)) 
+                count +=  map.get(sumUntilNow - k);
+            addToMap(sumUntilNow);
+        }
+        return count;
+    }
+
+    private void addToMap(int sum){
+        if(map.containsKey(sum)) map.put(sum ,map.get(sum)+1);
+        else map.put(sum ,1);
+    }
+}
+
+class subArrSumK {
+    public int subarraySum(int[] nums, int k) {
+
+        // have a prefix sum
+        // Complexity Analysis
+
+        // Time complexity : O(n
+        // 2
+        // ). Considering every possible subarray takes O(n
+        // 2
+        // ) time. Finding out the sum of any subarray takes O(1) time after the initial
+        // processing of O(n) for creating the cumulative sum array.
+
+        // Space complexity : O(n). Cumulative sum array sum of size n+1 is used
+
+        int[] prefixSum = new int[nums.length + 1];
+
+        prefixSum[1] = nums[0];
+        prefixSum[0] = 0;
+
+        for (int I = 1; I <= nums.length; I++) {
+            prefixSum[I] = nums[I - 1] + prefixSum[I - 1];
+            // System.out.println(prefixSum[I]);
+        }
+
+        int count = 0, start = 0, end = 0;
+
+        for (int windowSize = nums.length; windowSize > 0; windowSize--) {
+            start = 0;
+            end = windowSize - 1;
+
+            while (end < nums.length) {
+                // System.out.println("end "+end +" prefixSum[end+1] "+prefixSum[end+1] );
+                // System.out.println("start "+start +" prefixSum[start] "+prefixSum[start]);
+
+                if (prefixSum[end + 1] - prefixSum[start] == k) {
+                    // System.out.println("count "+count);
+                    count++;
+                }
+                end++;
+                start++;
+            }
+        }
+
+        return count;
+
+    }
+}
+
--- Neetcode-2023/Arrays&Hashing/validAnagram.java
+++ Neetcode-2023/Arrays&Hashing/validAnagram.java
@@ -2,6 +2,9 @@ import java.util.Arrays;
 
 class Solution {
     public boolean isAnagram(String s, String t) {
+
+
+
         if(s.length()!= t.length()) return false;
 
         int charMap[]=new int[26];
@@ -21,5 +24,37 @@ class Solution {
         }
 
         return true;
+    }
+}
+
+class SolutionWith26 {
+    public boolean isAnagram(String s, String t) {
+
+        // if(s.length() != t.length()) return false;
+        
+        // int[] charArr = new int[26];
+        // Arrays.fill(charArr, 0);
+
+        // char[] sArr = s.toCharArray();
+        // char[] tArr = t.toCharArray();
+
+        // for(int i =0;i< sArr.length;i++){
+        //     charArr[sArr[i]-'a']++;
+        //     charArr[tArr[i]-'a']--;
+        // }
+
+        // for(int i =0;i< s.length();i++){
+        //     charArr[s.charAt(i)-'a']++;
+        //     charArr[t.charAt(i)-'a']--;
+        // }
+
+        // for (int n : charArr) if (n != 0) return false;
+
+        // for(int j =0;j< charArr.length ; j++){
+        //     if (charArr[j] != 0 )return false;
+        // }
+
+        return true;
+
     }
 }
