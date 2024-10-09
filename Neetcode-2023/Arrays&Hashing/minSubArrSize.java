d7efd25b2052e5e89a24bb37a9d7471e93166b34
Rach1507 <rachitha715@gmail.com>
2024-10-09 21:54:55 +0530

    Variable Size sliding window

--- Neetcode-2023/Arrays&Hashing/minSubArrSize.java
+++ Neetcode-2023/Arrays&Hashing/minSubArrSize.java
@@ -1,5 +1,30 @@
 public class minSubArrSize {
 
+        public int minSubArrayLenOptimal(int target, int[] nums) {
+            int left = 0, right = 0, minLen = Integer.MAX_VALUE;
+            int total = nums[0];
+    
+            while (right <= nums.length - 1) {
+    
+                if (total >= target) {
+                    minLen = Math.min(minLen, right - left + 1);
+                    total = total - nums[left];
+                    left++;
+                    continue;
+                } else {
+                    right++;
+                }
+                if(right <= nums.length - 1)total += nums[right];
+    
+            }
+    
+            if (minLen == Integer.MAX_VALUE)
+                return 0;
+            return minLen;
+    
+        }
+
+
         public int minSubArrayLen(int target, int[] arr) {
             // positive intergers 
             // positive target 
