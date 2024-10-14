77c127ed48f251e37e7e9aa2032b0169e27c2a3a
Rach1507 <rachitha715@gmail.com>
2024-10-14 23:46:00 +0530

    lengthOfLongestSubstring optiomal

--- Neetcode-2023/String/Maxconsective.java
+++ Neetcode-2023/String/Maxconsective.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class Maxconsective {
     public int lengthOfLongestSubstring(String s) {
-
         // abwpwkew
         if (s.length() == 0)
             return 0;
@@ -31,7 +30,24 @@ public class Maxconsective {
             }
 
         }
-
         return maxLen;
+    }
+
+    public int lengthOfLongestSubstringOptimal(String s) {
+        int n = s.length();
+        int maxLength = 0;
+        int[] charIndex = new int[128];
+        Arrays.fill(charIndex, -1);
+        int left = 0;
+        
+        for (int right = 0; right < n; right++) {
+            if (charIndex[s.charAt(right)] >= left) {
+                left = charIndex[s.charAt(right)] + 1;
+            }
+            charIndex[s.charAt(right)] = right;
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        
+        return maxLength;
     }
 } 
